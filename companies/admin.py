be75170d77febadfcd0a10016e5fd6a2b75de9bb
Paweł Szrubas <pawel050673@gmail.com>
2025-03-23 16:37:18 +0100

    WT-29 added operator model

--- companies/admin.py
+++ companies/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Company
+from .models import Company, Operator
 # Register your models here.
 
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ("name", "email", "telephone")
-    
+
+class OperatorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "company")
+
 admin.site.register(Company, CompanyAdmin)
+admin.site.register(Operator, OperatorAdmin)
