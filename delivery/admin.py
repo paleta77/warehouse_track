f723941e165ca95d27de8ba34ce47104a6990b63
Paweł Szrubas <pawel050673@gmail.com>
2025-03-23 21:01:05 +0100

    WT-30 registered models in admin

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Delivery, Truck, Driver, Customer, Worker
 
 # Register your models here.
+admin.site.register(Delivery)
+admin.site.register(Truck)
+admin.site.register(Driver)
+admin.site.register(Customer)
+admin.site.register(Worker)
--- delivery/models.py
+++ delivery/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from warehouse.models import PayloadArea
+from warehouse.models import PayloadArea, Dock, Aisle, Warehouse
+from companies.models import Company
 from django.core.exceptions import ValidationError
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -15,6 +16,37 @@ class Driver(models.Model):
     telephone = PhoneNumberField()
     email = models.EmailField(max_length=255)
     trucks = models.ManyToManyField(Truck)
+    
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    telephone = PhoneNumberField()
+    email = models.EmailField(max_length=255)
+    
+class Worker(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    telephone = PhoneNumberField()
+    email = models.EmailField(max_length=255)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    work_start = models.TimeField()
+    work_end = models.TimeField()
+    
+DELIVERY_TYPES = {
+    "IN": "In",
+    "OUT": "Out"
+}
+
+class Delivery(models.Model):
+    delivery_number = models.CharField(max_length=255)
+    delivery_type = models.CharField(max_length=255, choices=DELIVERY_TYPES)
+    date = models.DateField()
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    workers = models.ManyToManyField(Worker)
+    truck = models.ForeignKey(Truck, on_delete=models.SET_NULL, null=True)
+    dock = models.ForeignKey(Dock, on_delete=models.SET_NULL, null=True)
+    aisle = models.ForeignKey(Aisle, on_delete=models.SET_NULL, null=True)
 
 STATUSES = {
     "in_warehouse": "In a warehouse",
