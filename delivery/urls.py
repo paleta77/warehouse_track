04a2aab8423b4e82cb331e95d90037005c56fa72
Paweł Szrubas <pawel050673@gmail.com>
2025-05-07 20:53:19 +0200

    WT-37 added package page

--- delivery/urls.py
+++ delivery/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
         views.delivery_detail,
         name="delivery_detail",
     ),
+    path(
+        "packages/<int:package_id>/",
+        views.package_detail,
+        name="package_detail",
+    ),
 ]
